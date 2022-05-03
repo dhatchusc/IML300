fae4ef5151dcde2840ecf5a5246386df08d6f6df
Darcy Hatcher <dlhatche@usc.edu>
2022-05-03 16:35:37 -0700

    gallery works

--- project-3/script.js
+++ project-3/script.js
@@ -1,9 +1,23 @@
-$(document).ready(function () {   
-$('#coolflower').click(function (){
-    $('p').toggle();
-});
-    $('#keyboardplant').click(function (){
-        $(this).toggleClass('bigger');
-})
-    $('#coolflower').draggable();
+$(document).ready(function(){
+
+
+	$(".element").click(function(){
+		
+		//---------------------------------------
+		// add 'play' class to activate animation
+		//---------------------------------------
+		$(this).toggleClass('play');
+	});
+
+	$('#toggle').click(function(){
+		$('.element').toggleClass('play');
+	});
+	$('#play').click(function(){
+		$('.element').addClass('play');
+	});
+	$('#pause').click(function(){
+		$('.element').removeClass('play');
+	});
+
+
 });
